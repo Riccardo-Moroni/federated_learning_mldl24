9ebcf205eed8da43c70bbe465c4e5dcf579098e7
Riccardo <moroniriccardo99@gmail.com>
2024-06-06 02:26:30 +0200

    centralized_shakespeare.py works

--- src/shakespeare/centralized_shakespeare.py
+++ src/shakespeare/centralized_shakespeare.py
@@ -59,52 +59,39 @@ with open('./pickles/Y_test.pkl', 'rb') as f:
 # %%
 train_sentence = ' '.join(X_train)
 vocab_train = sorted(set(train_sentence))
+vocab_train.append('<OOV>')
 
 char_to_idx = {char: idx for idx, char in enumerate(vocab_train)}
 idx_to_char = {idx: char for idx, char in enumerate(vocab_train)}
 
-# def substitute_oov(sentences, char_to_idx):
-#     substituted_sentences = []
-#     for s in sentences:
-#         new_s = ''.join([char if char in char_to_idx.keys() else oov for char in s])
-#         substituted_sentences.append(new_s)
-#     return substituted_sentences
-
-# X_test = substitute_oov(X_test, char_to_idx)
-# Y_test = substitute_oov(Y_test, char_to_idx)
-
 # %%
 # create a ./tensors/ folder in which to save the (encoded) tensors 
-# def encode(data, dict):
-#     x = []
-#     for i in tqdm(range(len(data))):
-#             x.append(np.array([dict[char] for char in data[i]]))
-#     return x
 
-def tokenize_encode(my_list, char_dict, oov_token='<OOV>'):
+def tokenize_encode(my_list, char_to_idx):
+    oov_token = len(vocab_train)-1
     new_list = []
     for sentence in tqdm(my_list):
         characters = list(sentence)
 
-        substituted_indices = []
+        encoded = []
         for char in characters:
-            if char in char_dict:
-                substituted_indices.append(char_dict[char])
+            if char in char_to_idx:
+                encoded.append(char_to_idx[char])
             else:
-                substituted_indices.append(oov_token)
+                encoded.append(oov_token)
     
-        new_list.append(substituted_indices)
+        new_list.append(encoded)
     return new_list
 
-X_train_enc = tokenize_encode(X_train, char_to_idx)
-Y_train_enc = tokenize_encode(Y_train, char_to_idx)
-X_test_enc = tokenize_encode(X_test, char_to_idx)
-Y_test_enc = tokenize_encode(Y_test, char_to_idx)
+X_train_enc = np.array(tokenize_encode(X_train, char_to_idx))
+Y_train_enc = np.array(tokenize_encode(Y_train, char_to_idx)).flatten()
+X_test_enc = np.array(tokenize_encode(X_test, char_to_idx))
+Y_test_enc = np.array(tokenize_encode(Y_test, char_to_idx)).flatten()
 
-X_train_tensor = torch.tensor(np.array(X_train_enc), dtype=torch.long)
-Y_train_tensor = torch.tensor(np.array(Y_train_enc), dtype=torch.long)
-X_test_tensor = torch.tensor(np.array(X_test_enc), dtype=torch.long)
-Y_test_tensor = torch.tensor(np.array(Y_test_enc), dtype=torch.long)
+X_train_tensor = torch.tensor(X_train_enc, dtype=torch.long)
+Y_train_tensor = torch.tensor(Y_train_enc, dtype=torch.long)
+X_test_tensor = torch.tensor(X_test_enc, dtype=torch.long)
+Y_test_tensor = torch.tensor(Y_test_enc, dtype=torch.long)
 
 # save tensors
 torch.save(X_train_tensor,'./tensors/X_train_tensor.pt')
@@ -125,9 +112,9 @@ test_dataset = TensorDataset(X_test_tensor, Y_test_tensor)
 
 # training parameters
 train_params = {
-    'batch_size' : 100,
+    'batch_size' : 150,
     'lr' : 1e-1,
-    'epochs' : 1,
+    'epochs' : 3,
     'momentum': 0.9,
 }
 
@@ -152,18 +139,18 @@ optimizer = torch.optim.SGD(model.parameters(), lr=train_params['lr'], momentum=
 test_freq = 50
 
 def train(model):
-    accuracies = []
-    losses = []
-    for t in tqdm(range(train_params['epochs'])):
+    accuracies, losses = [], []
+    for t in range(train_params['epochs']):
         model.train()
-
-        for batch_idx, (inputs, targets) in enumerate(train_loader):
+        for batch_idx, (inputs, targets) in enumerate(tqdm(train_loader)):
             inputs, targets = inputs.cuda(), targets.cuda()
             optimizer.zero_grad()
             outputs = model(inputs)
             loss = criterion(outputs, targets)
             loss.backward()
             optimizer.step()
+
+        torch.save(model.state_dict(), f"./saved_models/{train_params['epochs']}epochs_weights.pt")
 
         # test (after each single epoch)
         acc, loss = test(model)
@@ -195,68 +182,23 @@ def test(model):
     print(f'Test Loss: {test_loss:.6f} Acc: {test_accuracy:.2f}%')
     return test_accuracy, test_loss
 
+
 # %%
 accuracies, losses = train(model)
 
 # %%
-import os
-import json
-import torch
-from torch.utils.data import Dataset, DataLoader
-
-def load_json_files(directory):
-    data = []
-    for filename in os.listdir(directory):
-        if filename.endswith(".json"):
-            with open(os.path.join(directory, filename), 'r') as f:
-                client_data = json.load(f)
-                data.extend(client_data)
-    return data
-
-train_data = load_json_files('../../datasets/shakespeare/tensorflow_version/shakespeare_data/train')
-test_data = load_json_files('../../datasets/shakespeare/tensorflow_version/shakespeare_data/test')
-
-sequence_length = 80
-def create_sequences(data, sequence_length):
-    inputs, targets = [], []
-    for snippet in data:
-        text = snippet['text']
-        for i in range(len(text) - sequence_length):
-            inputs.append(text[i:i + sequence_length])
-            targets.append(text[i + sequence_length])
-    return inputs, targets
-
-train_inputs, train_targets = create_sequences(train_data, sequence_length)
-test_inputs, test_targets = create_sequences(test_data, sequence_length)
-
-# %%
-# Encode
-all_text = ''.join([snippet['text'] for snippet in train_data])
-vocab = sorted(set(all_text))
-char2idx = {char: idx for idx, char in enumerate(vocab)}
-idx2char = {idx: char for idx, char in enumerate(vocab)}
-
-def encode(text, char2idx):
-    return [char2idx[char] for char in text]
-
-train_inputs_enc = [encode(seq, char2idx) for seq in train_inputs]
-train_targets_enc = [char2idx[char] for char in train_targets]
-test_inputs_enc = [encode(seq, char2idx) for seq in test_inputs]
-test_targets_enc = [char2idx[char] for char in test_targets]
-
-class ShakespeareDataset(Dataset):
-    def __init__(self, inputs, targets):
-        self.inputs = inputs
-        self.targets = targets
-
-    def __len__(self):
-        return len(self.inputs)
-
-    def __getitem__(self, idx):
-        return torch.tensor(self.inputs[idx]), torch.tensor(self.targets[idx])
-
-train_dataset = ShakespeareDataset(train_inputs_enc, train_targets_enc)
-test_dataset = ShakespeareDataset(test_inputs_enc, test_targets_enc)
-
-train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+# testing one batch on cpu (debug)
+# model = CharRNN(vocab_size = model_params['vocab_size'], embed_dim = model_params['embed_dim'], lstm_units=model_params['lstm_units'])
+# model.load_state_dict(torch.load('./saved_models/1epochs_weights.pth'))
+# model.eval()
+# test_loss = 0
+# correct = 0
+# total = 0
+# for batch_idx, (inputs, targets) in enumerate(test_loader):
+#     outputs = model(inputs)
+#     loss = criterion(outputs, targets)
+#     test_loss += loss.item()
+#     _, predicted = outputs.max(1)
+#     total += targets.size(0)
+#     correct += predicted.eq(targets).sum().item()
+#     break
